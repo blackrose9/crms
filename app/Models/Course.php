a0d785c8a0a8db739705ac0d6a2b3356c918af27
blackrose9 <sarah.mwendwa@outlook.com>
2020-10-02 15:59:11 +0200

    Define one to many relationship between User and Course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
     use HasFactory;
 }
